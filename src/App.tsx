7570d4e2778b74f72735749973540682cfded743
Matevž <matevz5.kovacic@gmail.com>
2025-02-24 16:42:45 +0100

    added title and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import SeasonSwitch from "./components/seasonswitch/SeasonSwitch";
 import Timetable from "./components/timetable/Timetable";
+import getUrnikFriUrl from "./functions/getUrnikFriUrl";
+import useLocalStorage from "./hooks/useLocalStorage";
 
+export type Season = "letni" | "zimski" | undefined;
 
-function App() {
-	return <Timetable></Timetable>;
+export default function App() {
+	//const [urnikFriSeasonalPartOfUrl, setUrnikFriSeasonalPartOfUrl] = useState<Season>();
+	const [urnikFriSeasonalPartOfUrl, setUrnikFriSeasonalPartOfUrl] = useLocalStorage("", null);
+
+	useEffect(() => {
+		const fetchTimetableFromUrnikFRI = (url: string) => {};
+	}, []);
+
+	return (
+		<>
+			<SeasonSwitch
+				urnikFriSeasonalPartOfUrl={urnikFriSeasonalPartOfUrl}
+				setUrnikFriSeasonalPartOfUrl={setUrnikFriSeasonalPartOfUrl}
+			/>
+			{getUrnikFriUrl(urnikFriSeasonalPartOfUrl, 63240167)}
+			<Timetable />
+		</>
+	);
 }
-
-export default App;
