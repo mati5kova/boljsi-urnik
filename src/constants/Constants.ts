f8beb69fd5e8604a45e09c2c8db9a0aa35140403
Matevž <matevz5.kovacic@gmail.com>
2025-02-25 18:45:42 +0100

    added constants file

---
+++ src/constants/Constants.ts
@@ -0,0 +1,8 @@
+import { LecturesAuditoryAndLaboratoryExcersises } from "../context/BoljsiUrnikContext";
+
+export const defaultLecturesAuditoryAndLaboratoryExcersisesObject: LecturesAuditoryAndLaboratoryExcersises = {
+	dateOfRequest: new Date("1-1-1900"),
+	lecturesP: [],
+	lecturesAV: [],
+	lecturesLV: [],
+};
