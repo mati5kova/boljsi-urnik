d38b743a6631d56cffba827aa73b789c310de5f3
Matevž <matevz5.kovacic@gmail.com>
2025-02-26 20:25:46 +0100

    improved conditional rendering, removed redundant logic

--- src/components/timetable/DayColumn.tsx
+++ src/components/timetable/DayColumn.tsx
@@ -16,22 +16,16 @@ export default function DayColumn({ i, nameOfDay, gridAreaName }: DayColumn) {
 			<div className="grid-day" style={{ gridColumn: `${i}` }}>
 				{nameOfDay}
 			</div>
-			<div className="grid-day-column" style={{ gridArea: `${gridAreaName}` }}>
-				{lecturesAuditoryAndLaboratoryExcersises.lecturesP.map((laale) => {
-					if (laale.gridArea === gridAreaName) {
-						return <Lecture key={laale.lectureName} {...laale} />;
-					}
-				})}
-				{lecturesAuditoryAndLaboratoryExcersises.lecturesAV.map((laale) => {
-					if (laale.gridArea === gridAreaName) {
-						return <Lecture key={laale.lectureName} {...laale} />;
-					}
-				})}
-				{lecturesAuditoryAndLaboratoryExcersises.lecturesLV.map((laale) => {
-					if (laale.gridArea === gridAreaName) {
-						return <Lecture key={laale.lectureName} {...laale} />;
-					}
-				})}
+			<div className="grid-day-column" style={{ gridArea: gridAreaName }}>
+				{[
+					...lecturesAuditoryAndLaboratoryExcersises.lecturesP,
+					...lecturesAuditoryAndLaboratoryExcersises.lecturesAV,
+					...lecturesAuditoryAndLaboratoryExcersises.lecturesLV,
+				]
+					.filter((laale) => laale.gridArea === gridAreaName)
+					.map((laale) => (
+						<Lecture key={laale.lectureName} {...laale} />
+					))}
 			</div>
 		</>
 	);
