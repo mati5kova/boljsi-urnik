4d5c3a6c16e8c27801a4a19af9d623eff06e89e7
Matevž <matevz5.kovacic@gmail.com>
2025-03-03 23:11:53 +0100

    small changes for netlify

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./Index.css";
 import { BoljsiUrnikProvider } from "./context/BoljsiUrnikContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
