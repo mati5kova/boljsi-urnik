852014af4d67a9e3a9da6f32e02d22f343a13549
Matevž Kovačič <matevz5.kovacic@gmail.com>
2025-09-04 18:57:15 +0200

    solsko leto se spremeni ze septembra

--- src/functions/getCurrentSchoolYear.ts
+++ src/functions/getCurrentSchoolYear.ts
@@ -10,10 +10,10 @@ export default function getCurrentSchoolYear(): [number, number] {
 	// če do funkcije dostopamo med oktobrom in decembrom je prvi element trenutno leto, drugi element pa naslednje leto
 	//
 	// če do funkcije dostopamo ostale mesece v letu je prvi element prejšnje leto, drugi pa trenutno leto
-	if ([10, 11, 12].includes(month)) {
+	if ([9, 10, 11, 12].includes(month)) {
 		schoolYear[0] = today.getFullYear();
 		schoolYear[1] = today.getFullYear() + 1;
-	} else if ([1, 2, 3, 4, 5, 6, 7, 8, 9].includes(month)) {
+	} else if ([1, 2, 3, 4, 5, 6, 7, 8].includes(month)) {
 		schoolYear[0] = today.getFullYear() - 1;
 		schoolYear[1] = today.getFullYear();
 	}
